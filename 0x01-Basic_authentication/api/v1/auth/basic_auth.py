59c7a28ec16ed51f34b3f5fd3e58e9d8ab505cbc
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-08-09 13:15:12 +0000

    post Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -55,3 +55,27 @@ class BasicAuth(Auth):
             return decoded_value
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """
+        Extract user email and password from the Base64 decoded value.
+
+        :param decoded_base64_authorization_header: The decoded Base64
+        authorization header value.
+        :type decoded_base64_authorization_header: str
+        :return: A tuple containing user email and user password.
+        :rtype: tuple
+        """
+        if decoded_base64_authorization_header is None or not isinstance(
+                decoded_base64_authorization_header, str
+                ):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
