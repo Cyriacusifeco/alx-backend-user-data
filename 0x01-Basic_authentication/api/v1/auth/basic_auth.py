48a10e11fdc487992052a6c06019b193b700e2dc
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-08-09 13:05:00 +0000

    decode base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 basic auth module
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -29,3 +30,28 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+            ) -> str:
+        """
+        Decode a Base64 authorization header value to a UTF8 string.
+
+        :param base64_authorization_header: The Base64 authorization
+        header value.
+        :type base64_authorization_header: str
+        :return: The decoded value as UTF8 string.
+        :rtype: str
+        """
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header,
+                str
+                ):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_value = decoded_bytes.decode('utf-8')
+            return decoded_value
+        except base64.binascii.Error:
+            return None
